88db940cfc5c1cdf6f074b100c54a1245f6d60be
kaemic <kaemic@gmail.com>
2015-12-21 00:55:05 +0100

    usage for y2mp3

--- youtube2mp3.sh
+++ youtube2mp3.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+
+# usage: ./youtube2mp3.sh https://www.youtube.com/watch?v=dQw4w9WgXcQ
 youtube-dl --output "/home/keymic/Downloads/youtube/%(title)s.%(ext)s" -f bestaudio --extract-audio --audio-format mp3 "$1"
